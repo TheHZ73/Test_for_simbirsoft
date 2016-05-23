995bac50a0d2df45a8abf836d8968be514b3ef77
TheHZ73 <dddd73@yandex.ru>
2016-05-24 02:11:51 +0400

    Test

---
+++ FindWord_in_File/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace FindWord_in_File
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CreateDocButton = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.OpenDict = new System.Windows.Forms.Button();
+            this.PuthDictionary = new System.Windows.Forms.Label();
+            this.OpenProvFail = new System.Windows.Forms.Button();
+            this.PuthTestFile = new System.Windows.Forms.Label();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.KolStrokHtml = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.KolStrokHtml)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CreateDocButton
+            // 
+            this.CreateDocButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.CreateDocButton.Location = new System.Drawing.Point(25, 282);
+            this.CreateDocButton.Name = "CreateDocButton";
+            this.CreateDocButton.Size = new System.Drawing.Size(480, 23);
+            this.CreateDocButton.TabIndex = 0;
+            this.CreateDocButton.Text = "Сформировать HTML-документ";
+            this.CreateDocButton.UseVisualStyleBackColor = true;
+            this.CreateDocButton.Click += new System.EventHandler(this.CreateDocButton_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // OpenDict
+            // 
+            this.OpenDict.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.OpenDict.Location = new System.Drawing.Point(20, 109);
+            this.OpenDict.Name = "OpenDict";
+            this.OpenDict.Size = new System.Drawing.Size(166, 42);
+            this.OpenDict.TabIndex = 1;
+            this.OpenDict.Text = "Выбрать словарь";
+            this.OpenDict.UseVisualStyleBackColor = true;
+            this.OpenDict.Click += new System.EventHandler(this.OpenDict_Click);
+            // 
+            // PuthDictionary
+            // 
+            this.PuthDictionary.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.PuthDictionary.AutoSize = true;
+            this.PuthDictionary.Location = new System.Drawing.Point(22, 166);
+            this.PuthDictionary.Name = "PuthDictionary";
+            this.PuthDictionary.Size = new System.Drawing.Size(0, 13);
+            this.PuthDictionary.TabIndex = 2;
+            // 
+            // OpenProvFail
+            // 
+            this.OpenProvFail.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.OpenProvFail.Location = new System.Drawing.Point(20, 12);
+            this.OpenProvFail.Name = "OpenProvFail";
+            this.OpenProvFail.Size = new System.Drawing.Size(166, 42);
+            this.OpenProvFail.TabIndex = 3;
+            this.OpenProvFail.Text = "Выбрать проверяемый файл";
+            this.OpenProvFail.UseVisualStyleBackColor = true;
+            this.OpenProvFail.Click += new System.EventHandler(this.PutProvFail_Click);
+            // 
+            // PuthTestFile
+            // 
+            this.PuthTestFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.PuthTestFile.AutoSize = true;
+            this.PuthTestFile.Location = new System.Drawing.Point(22, 69);
+            this.PuthTestFile.Name = "PuthTestFile";
+            this.PuthTestFile.Size = new System.Drawing.Size(0, 13);
+            this.PuthTestFile.TabIndex = 4;
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // KolStrokHtml
+            // 
+            this.KolStrokHtml.Location = new System.Drawing.Point(25, 241);
+            this.KolStrokHtml.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.KolStrokHtml.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.KolStrokHtml.Name = "KolStrokHtml";
+            this.KolStrokHtml.Size = new System.Drawing.Size(129, 20);
+            this.KolStrokHtml.TabIndex = 5;
+            this.KolStrokHtml.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 214);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(244, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Количество строк в формируемых html файлах";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(517, 338);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.KolStrokHtml);
+            this.Controls.Add(this.PuthTestFile);
+            this.Controls.Add(this.OpenProvFail);
+            this.Controls.Add(this.PuthDictionary);
+            this.Controls.Add(this.OpenDict);
+            this.Controls.Add(this.CreateDocButton);
+            this.MinimumSize = new System.Drawing.Size(533, 376);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.KolStrokHtml)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CreateDocButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button OpenDict;
+        private System.Windows.Forms.Label PuthDictionary;
+        private System.Windows.Forms.Button OpenProvFail;
+        private System.Windows.Forms.Label PuthTestFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.NumericUpDown KolStrokHtml;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ FindWord_in_File/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+//автор Яковлев Дмитрий Александрович
+//сотовый 89176203326
+//e-mail dddd73@yandex.ru
+namespace FindWord_in_File
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            PuthDictionary.Text = @"словарь.txt";
+            PuthTestFile.Text = @"проверяемый файл.txt";
+        }
+        //создание html документа
+        private void CreateDocButton_Click(object sender, EventArgs e)
+        {
+            string Expansion1 = Path.GetExtension(PuthDictionary.Text), Expansion2 = Path.GetExtension(PuthTestFile.Text);
+
+            if ((File.Exists(PuthDictionary.Text)) && (File.Exists(PuthTestFile.Text)))
+            {
+                if ((Expansion1 == ".txt") && (Expansion2 == ".txt"))
+                {
+                    FileInfo file = new FileInfo(PuthTestFile.Text);
+                    if (file.Length < 2000000)
+                    {
+                        if (File.ReadAllLines(PuthDictionary.Text).Length < 100000)
+                        {
+                            var words = File.ReadLines(PuthDictionary.Text, Encoding.Default).Select(i => i.Trim()).ToArray();
+
+
+                            string result = " ";
+                            int Counter_N = 0, z = 0;
+                            using (var reader = new StreamReader(PuthTestFile.Text, Encoding.Default))
+                            {
+                                while (!reader.EndOfStream)
+                                {
+                                    var w = reader.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+                                    for (int i = 0; i < w.Length; i++)
+                                    {
+                                        if (words.Contains(w[i]))
+                                        {
+                                            result += @"<b><i>" + w[i] + @"</i></b>" + " ";
+                                        }
+                                        else
+                                        {
+                                            result += w[i] + " ";
+                                        }
+                                    }
+
+                                    Counter_N++;
+                                    result += "<br/>";
+                                    if (Counter_N == KolStrokHtml.Value)
+                                    {
+                                        string header = @"<!DOCTYPE html>
+                                    <html>
+                                    <head>
+                                    <title>Прочитанный файл</title>
+                                    <meta http-equiv=""content-type"" content=""text/html; charset=windows-1251"" />
+                                    </head>
+                                    <body>
+                                    <div style=""margin:30px"">
+                                    <p>" + result + @"</p>
+                                    </div>
+                                    </body>
+                                    </html> ";
+                                        File.WriteAllText("default" + z + ".html", header, Encoding.Default);
+                                        z++;
+                                        result = "";
+                                        Counter_N = 0;
+                                    }
+                                }
+                                if (z == 0)
+                                {
+                                    string header = @"<!DOCTYPE html>
+                                    <html>
+                                    <head>
+                                    <title>Прочитанный файл</title>
+                                    <meta http-equiv=""content-type"" content=""text/html; charset=windows-1251"" />
+                                    </head>
+                                    <body>
+                                    <div style=""margin:30px"">
+                                    <p>" + result + @"</p>
+                                    </div>
+                                    </body>
+                                    </html> ";
+                                    File.WriteAllText("default.html", header, Encoding.Default);
+                                    MessageBox.Show("Файл сформирован и лежит в корневой папке под именем default.html");
+                                }
+
+                                if (z >= 1)
+                                    MessageBox.Show("Файлы сфорированы и лежат в корневой папке под именами default0.html, default1.html...");
+                            }
+                        }
+                        else MessageBox.Show("Строк в файле словаря более 100 тысяч");
+                    }
+                    else MessageBox.Show("Размер проверочного файла больше 2 Мб");
+                }
+                else MessageBox.Show("Расширение файла словаря или проверямого файла не .txt");
+            }
+            else MessageBox.Show("Не найден файл словаря или проверяемый файл");
+        }
+
+        //выбирает файл словаря
+        private void OpenDict_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                PuthDictionary.Text=openFileDialog1.FileName;
+            }
+        }
+        
+        //выбирает файл с текстом
+        private void PutProvFail_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog2.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                PuthTestFile.Text = openFileDialog2.FileName;
+            }
+        }
+    }
+}
